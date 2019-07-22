1b4ce923c70c3262a4e52377038cffb21831a934
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-22 13:50:13 -0400

    adds challenges

--- main.js
+++ main.js
@@ -1,69 +1,56 @@
-console.log('Challenge #1');
+const classroom = ['Colin', 'Mesuara'];
 
-// Console out the numbers from 1-20, but only if they're divisible by 3.
+console.log('# Challenge #1');
 
-// Use `if` and a modulus operation.
+// Manually add every student BUT yourself to the above, and console out how many are in there, using `length`.
 
+console.log('# Challenge #2');
 
-console.log('Challenge #2');
+// Now use `push` to get yourself in there, and console out the new length.
 
-// Same problem as #1, but no `if`s, `mods`, or `but`s (and no, that last one is
-// not a thing). If you did it right, you should run the loop 6 times with 1
-// console log per loop. That's called... a clue!
+console.log('# Challenge #3');
 
+// Now console out the 1st person. It should be 'Colin'!
 
-console.log('Challenge #3');
+console.log('# Challenge #4');
 
-// Console out the numbers from 1-20. BACKWARDS.
+// Console out the 5th person. Don't forget to start counting at 0!
 
-// (And, optionally, in heels.)
+console.log('# Challenge #5');
 
-// (That's a reference that only Olds make, and only if they're SUPER CULTURED.)
+// Now console out the last, but use `length` to find it.
 
-// (No extra credit will be given for wearing heels during this challenge.)
+console.log('# Challenge #6');
 
+// Now console out the next to last, still using `length`.
 
-console.log('Challenge #4');
+console.log('# Challenge #7');
 
-// For the string 'Boy howdy am I good at this!', console out the letters
-// individually.
+// Remove the last person and console out the new length of the array.
 
+console.log('# Challenge #8');
 
-console.log('Challenge #5');
+// Now console out the person you just removed. You might have to change the code from the previous # challenge to make this possible!
 
-// For the string, 'And getting better every day.', starting with the second
-// character, print out every third character.
+console.log('# Challenge #9');
 
-
-console.log('Challenge #6');
-
-// For the string 'Am I the best?', console out the letters individually, AND
-// UPPERCASED.
+// Now remove and console out the name of the _first_ person on the list.
 
 
-console.log('Challenge #7');
+console.log('# Challenge #10');
 
-// For the string 'Whoa, I am the best!', console out the letters individually,
-// but also capitalize the letters in the third word.
+// Now remove and console out the name of the _new_ first person on the list.
 
+console.log('# Challenge #11');
 
-console.log('Challenge #8');
+// Now add 'Ginny' to the _start_ of the list.
 
+console.log('# Challenge #11');
 
-// For the string 'I am become death, destroyer of worlds.', console out the letters
-//  individually, but backwards. That is, the first console log should be
-// `.`, followed by `s`, followed by `d`, and so on.
+// Now add 'Yuri' to the start of the list.
 
+console.log('# Challenge #12');
 
-console.log('Challenge #9');
+// Now console out the whole list. You should only need one `console.log`!
 
-const names = 'Colin|Mesuara|Drew|In Seop|Emma|Ben|J|Ryan|Olgy|Mike|Chris|John';
-
-// Console out the individual letters of each person in class today, which are
-// in the string above. Every time you start a new name (including the first
-// one!), console out the string: 'Here today is:'.
-
-// You'll know you're at a new person's name when you hit the pipe character.
-
-// Don't print the pipes!
-
+// It should be the entire list of students minus yourself, with 'Colin' and 'Mesuara' replaced with 'Yuri' and 'Ginny'. In other words, the list of people you'll be around in Term 2!
